33d952e3ad7cbb05abdf9201a019b84322d872e0
Shoumikh <ssoumikh@gmail.com>
2020-06-22 17:39:44 +0600

    Updates

---
+++ precache-manifest.9f49ddd9a81fa85731f3a12923b27fd7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c657e7708b4eb0f3b80268be519c6860",
+    "url": "/monster-bots/index.html"
+  },
+  {
+    "revision": "621399c44ca4d89d41ed",
+    "url": "/monster-bots/static/css/main.e8c151d0.chunk.css"
+  },
+  {
+    "revision": "28f2cf9247d12a11ab18",
+    "url": "/monster-bots/static/js/2.9c487401.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-bots/static/js/2.9c487401.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "621399c44ca4d89d41ed",
+    "url": "/monster-bots/static/js/main.db155ff5.chunk.js"
+  },
+  {
+    "revision": "e3c69befead08533bbff",
+    "url": "/monster-bots/static/js/runtime-main.3d1b8fd9.js"
+  }
+]);
